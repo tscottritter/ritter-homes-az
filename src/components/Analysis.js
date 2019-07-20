33179a08952b2b67e322be4549d7fab94f267000
“Tyler <tyler.s.ritter@vanderbilt.edu>
2019-07-20 16:32:56 -0400

    rough start on contact form

--- src/components/Analysis.js
+++ src/components/Analysis.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import './Analysis.css';
 
 export default () => (
   <div className="Analysis">
-    this is my Analysis
+    <form accept-charset="utf-8" action="https://formspree.io/scottritteraz@gmail.com" method="post">
+      <div class="form-group">
+        <label for="full-name">Name</label>
+        <input type="text" class="form-control" id="full-name" required />
+      </div>
+      <div class="form-group">
+        <label for="email-address">Email Address</label>
+        <input type="email" class="form-control" name="_replyto" id="email-address" required />
+      </div>
+      <div class="form-group">
+        <label for="phone-number">Phone Number</label>
+        <input type="text" class="form-control" id="phone-number" required />
+      </div>
+      <input type="hidden" name="_subject" id="email-subject" value="Market Analysis Form Submission"></input>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
   </div>
 );
