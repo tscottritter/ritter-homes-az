fabbb420428393dd93535bb7ce406d85ca16cb36
“Tyler <tyler.s.ritter@vanderbilt.edu>
2019-07-28 15:27:51 -0400

    market watch page under construction text

--- src/components/Market.js
+++ src/components/Market.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default () => (
-  <div className="Market">
-    this is my Market
+  <div className="Market" style={{ marginBottom: 400+"px"}}>
+    This page is currently under construction, please check again soon!
   </div>
 );
