e8589c8da58a58abb6fa32a64eca0074f205b96b
“Tyler <tyler.s.ritter@vanderbilt.edu>
2019-10-21 20:14:00 -0400

    more video testimonials, updated recent sales, archive of market reports

--- src/components/Market.js
+++ src/components/Market.js
@@ -2,11 +2,26 @@ import React from 'react';
 import './Market.css';
 
 export default () => (
-  <div className="Market">
-    <img src="/images/July_chart.jpg" alt="the median home price in Mesa, AZ ia up 9.7% year over year"/>
-    <embed src="/images/July.pdf" type="application/pdf"></embed>
-    <embed src="/images/May.pdf" type="application/pdf"></embed>
-    <a href="/images/July.pdf" target="_blank" rel="noopener noreferrer">Click to view detailed July report</a>
-    <a href="/images/May.pdf" target="_blank" rel="noopener noreferrer">Click to view detailed May report</a>
+  <div className="Market row">
+    <div className="offset-lg-2 col-lg-8">
+      <img src="/images/Oct_chart.jpg" alt="Asking Prices up 9% Over Last Year, but are Buyers Paying It? These Homes Have Appreciated the Most since 2000"/>
+      <p>
+        <b>Asking Prices up 9% Over Last Year, but are Buyers Paying It? These Homes Have Appreciated the Most since 2000</b>
+        <br></br><br></br>
+        <b>For Buyers:</b> The news media is filled with short-term predictions regarding the economy and how it will, or will not, affect real estate prices.  It’s understandable for buyers to want their home to appreciate in value after they purchase, who doesn’t?  However there is far too much attention paid to short-term influences and fluctuations these days and not enough attention paid to the long view.  Real estate is a long-term investment for many people.  Despite the euphoria of 2005-2007 and the nightmare of 2008-2011, on average homes are selling 81.6% higher today than they were in the year 2000.  That’s an average appreciation rate of 4.3% per year over the course of 19 years.  Smaller homes appreciated the most over time while larger homes appreciated the least.  Homes under 1,000sf have appreciated 122% since 2000, an average of 6.4% per year.  Those between 1,000-2,000sf appreciated 106%, an average of 5.6% per year.  2,000-3,000sf appreciated 68% at 3.6% per year.  3,000-4,000sf appreciated 49% at 2.6% per year and homes over 4,000sf appreciated 11% at 0.6% per year. 
+        <br></br><br></br>
+        <b>For Sellers:</b> Average asking prices per square foot are up 9% over this time last year and they’re continuing to rise. However, not one individual price range has risen 9% or more; confusing, right? That’s because the sharp increase in the average has more to do with a growing market share of luxury active listings over $500K as inventory has plummeted everywhere else. The highest increase is within $200K-$250K, where sellers are asking 5.6% more than they were last year. That’s followed by listings over $1M where they’re asking 4.2% more and $500K-$1M at 4.0%.  All other price ranges are just 1-3% higher.  But are buyers paying?  Actually, many of them are! In the $200K-$250K range, the average sales price per square foot is still 0.8% higher than the average list price; and between $250-$300K the average sales price is 6.8% higher than the average list per square foot. Things change over $500K.  Between $500K-$1M there’s a -6.3% gap between asking price and sales price and over $1M the average sales price is -15.1% below the average asking price.  
+        <br></br><br></br>
+        Commentary written by Tina Tamboer, Senior Housing Analyst with The Cromford Report ©2019 Cromford Associates LLC and Tamboer Consulting LLC
+      </p>
+      <embed src="/images/Sep.pdf" type="application/pdf"></embed>
+      <a href="/images/Sep.pdf" target="_blank" rel="noopener noreferrer">Click here to see this month's detailed report</a><br></br>
+    </div>
+    <div className="col-lg-2">
+      <h5>Past Reports</h5>
+      <a href="/images/Aug.pdf" target="_blank" rel="noopener noreferrer">August 2019</a><br></br>
+      <a href="/images/July.pdf" target="_blank" rel="noopener noreferrer">July 2019</a><br></br>
+      <a href="/images/May.pdf" target="_blank" rel="noopener noreferrer">May 2019</a><br></br>
+    </div>
   </div>
 );
