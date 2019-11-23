acedaa4532e079bdc5206ac9d3294b695f0de341
“Tyler <tyler.s.ritter@vanderbilt.edu>
2019-11-22 19:51:49 -0500

    updated market analysis for november

--- src/components/Market.js
+++ src/components/Market.js
@@ -4,21 +4,30 @@ import './Market.css';
 export default () => (
   <div className="Market row">
     <div className="offset-lg-2 col-lg-8">
-      <img src="/images/Oct_chart.jpg" alt="Asking Prices up 9% Over Last Year, but are Buyers Paying It? These Homes Have Appreciated the Most since 2000"/>
+      <img src="/images/Nov_chart.jpg" alt="Asking Prices up 9% Over Last Year, but are Buyers Paying It? These Homes Have Appreciated the Most since 2000"/>
       <p>
-        <b>Asking Prices up 9% Over Last Year, but are Buyers Paying It? These Homes Have Appreciated the Most since 2000</b>
+        <b>Contracts In Escrow Up 19% Over Last Year Despite Rising Prices, Affordability is Good</b>
         <br></br><br></br>
-        <b>For Buyers:</b> The news media is filled with short-term predictions regarding the economy and how it will, or will not, affect real estate prices.  It’s understandable for buyers to want their home to appreciate in value after they purchase, who doesn’t?  However there is far too much attention paid to short-term influences and fluctuations these days and not enough attention paid to the long view.  Real estate is a long-term investment for many people.  Despite the euphoria of 2005-2007 and the nightmare of 2008-2011, on average homes are selling 81.6% higher today than they were in the year 2000.  That’s an average appreciation rate of 4.3% per year over the course of 19 years.  Smaller homes appreciated the most over time while larger homes appreciated the least.  Homes under 1,000sf have appreciated 122% since 2000, an average of 6.4% per year.  Those between 1,000-2,000sf appreciated 106%, an average of 5.6% per year.  2,000-3,000sf appreciated 68% at 3.6% per year.  3,000-4,000sf appreciated 49% at 2.6% per year and homes over 4,000sf appreciated 11% at 0.6% per year. 
+        <b>For Buyers:</b> Buyers waiting for prices to come down have been sorely disappointed so far in 2019.  The average sale price per square foot is up 6.7% since last November and the median sales price is now $283,000, up $21,000 from last November’s measure of $262,000.
         <br></br><br></br>
-        <b>For Sellers:</b> Average asking prices per square foot are up 9% over this time last year and they’re continuing to rise. However, not one individual price range has risen 9% or more; confusing, right? That’s because the sharp increase in the average has more to do with a growing market share of luxury active listings over $500K as inventory has plummeted everywhere else. The highest increase is within $200K-$250K, where sellers are asking 5.6% more than they were last year. That’s followed by listings over $1M where they’re asking 4.2% more and $500K-$1M at 4.0%.  All other price ranges are just 1-3% higher.  But are buyers paying?  Actually, many of them are! In the $200K-$250K range, the average sales price per square foot is still 0.8% higher than the average list price; and between $250-$300K the average sales price is 6.8% higher than the average list per square foot. Things change over $500K.  Between $500K-$1M there’s a -6.3% gap between asking price and sales price and over $1M the average sales price is -15.1% below the average asking price.  
+        Despite rising prices, affordability has remained normal throughout the year.  One relevant factor is Private Sector Earnings in Greater Phoenix has risen 4.5% annually as interest rates have continually declined.  The median family income was measured at $72,900 last quarter and families making that income could afford 68% of what sold last quarter (according to the HOI index published by the National Association of Home Builders and Wells Fargo).  The historical norm for our market is 60-75%.
+        <br></br><br></br>
+        Clearly not all buyers have parked on the fence, demand has been hovering 6-7% above normal for our area for about 4 months while supply is 44% below normal.  The only measurable relief for buyers is last month’s supply level was 47% below normal, so it’s 3% less hard to find something suitable.
+        <br></br><br></br>
+        <b>For Sellers:</b> The number of listings under contract may have declined 26% from its May seasonal peak, but it’s nearly 19% higher than it was this time last year.  This, combined with monthly sales up nearly 15% over last year, is a solid indicator that year-end closings will outperform last year despite a shaky start.
+        <br></br><br></br>
+        Single Family permits (future supply) are up 4.6% year-to-date and multi-family permits are up 6.4%, reaching a level not seen since 2007. Single family home sales are up 5.7%, but new townhome and condo sales are down a whopping 30%, which is surprising.  Resale condos and townhomes have increased in sales volume this year, so the drop in sales for new construction despite an increase in permits indicates that much of the multi-family units constructed are not for individual sale but are for rent.
+        <br></br><br></br>
+        This is good news if you’re planning to sell your condo because the majority of developments are not competing for buyers.  This is not good news if you’re renting your condo nearby because that’s an increase in competing units for renters.  “Apartment style” private condo rental rates per square foot have grown less than 1% over the course of 3.5 years according to the Arizona Regional MLS records.
         <br></br><br></br>
         Commentary written by Tina Tamboer, Senior Housing Analyst with The Cromford Report ©2019 Cromford Associates LLC and Tamboer Consulting LLC
       </p>
-      <embed src="/images/Sep.pdf" type="application/pdf"></embed>
-      <a href="/images/Sep.pdf" target="_blank" rel="noopener noreferrer">Click here to see this month's detailed report</a><br></br>
+      <embed src="/images/Oct.pdf" type="application/pdf"></embed>
+      <a href="/images/Oct.pdf" target="_blank" rel="noopener noreferrer">Click here to see this month's detailed report</a><br></br>
     </div>
     <div className="col-lg-2">
       <h5>Past Reports</h5>
+      <a href="/images/Sep.pdf" target="_blank" rel="noopener noreferrer">September 2019</a><br></br>
       <a href="/images/Aug.pdf" target="_blank" rel="noopener noreferrer">August 2019</a><br></br>
       <a href="/images/July.pdf" target="_blank" rel="noopener noreferrer">July 2019</a><br></br>
       <a href="/images/May.pdf" target="_blank" rel="noopener noreferrer">May 2019</a><br></br>
