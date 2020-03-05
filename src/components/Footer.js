4128c2952fd5c778d7c011dc48f67d1f35998408
“Tyler <tyler.s.ritter@vanderbilt.edu>
2020-03-04 20:47:45 -0500

    new brokerage address in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './Footer.css'
 
- function Footer() {
+function Footer() {
   let now = new Date();
 
   return (
     <div className="Footer">
       <p>
-        J&F Associates<br></br>
-        6197 S Rural Rd<br></br>
-        Tempe, AZ 85283<br></br>
+        Austin Fleck Property Management<br></br>
+        690 E Warner Rd #153<br></br>
+        Gilbert, AZ 85296<br></br>
       </p>
       <img src="/images/fairhousing.png" alt="Realtor MLS Equal Housing Opportunity"/>
       <p>&copy;{ now.getFullYear() } | <a href="https://www.linkedin.com/in/tyler-s-ritter/" target="_blank" rel="noopener noreferrer">TR</a></p>
     </div>
   )
- };
+};
 
 export default Footer;
