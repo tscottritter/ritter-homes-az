313802cf5a4035ba4a9f5395def2e580d68e3553
“Tyler <tyler.s.ritter@vanderbilt.edu>
2020-03-04 20:45:37 -0500

    new brokerage logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import './Header.css';
 
 export default () => (
   <div className="Header clearfix">
-    <img className="headshot" src="/images/headshot.png" alt="Scott Ritter professional headshot" />
-    <img className="logo" src="/images/jf_logo.png" alt="J and F Associates logo"/>
+    <img className="headshot" src="/images/headshot.png" alt="" />
+    <img className="logo" src="/images/fleck_logo.png" alt=""/>
     <div className="banner-headlines">
       <h1>Frederick Scott Ritter, Realtor&reg;</h1>
       <h3>Your trusted real estate expert</h3>
