b017b549e0e4f0e961594e3e52f712e375cd428d
Tyler Ritter <tyler.ritter@lumi.com.au>
2023-09-23 09:02:13 +1000

    new brokerage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,9 @@ function Footer() {
   return (
     <div className="Footer">
       <p>
-        JPAR Vantage<br></br>
-        2168 E Williams Field Rd, Suite 210<br></br>
-        Gilbert, AZ 85295<br></br>
+        Keller Williams Integrity First<br></br>
+        830 S Higley Rd<br></br>
+        Gilbert, AZ 85296<br></br>
       </p>
       <img src="/images/fairhousing.png" alt="Realtor MLS Equal Housing Opportunity"/>
       <p>&copy;{ now.getFullYear() } | <a href="https://www.linkedin.com/in/tyler-s-ritter/" target="_blank" rel="noopener noreferrer">TR</a></p>
